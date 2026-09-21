f1b7af5b9b758927e694104f941e31fe9c1096ae
Sanjay Krishna N <krisanjay16@gmail.com>
2026-09-21 21:09:55 +0530

    Spring Profile & Actuator

---
+++ src/test/java/org/secured/securitymanager/SecurityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.secured.securitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
